c4711b5d91bb61737a58ba28abfa5883e2e0b3fb
gw1402535 <kis@gabia.com>
2019-07-08 17:55:12 +0900

    react study

--- src/InputSample.js
+++ src/InputSample.js
@@ -1,21 +1,50 @@
-import React, {useState} from 'react';
+import React, { useState, useRef } from 'react';
 
 function InputSample() {
-  const [text, setText] = useState('');
-  
+  const [inputs, setInputs] = useState({
+    name: '',
+    nickname: ''
+  });
+
+  const nameInput = useRef();
+
+  const { name, nickname } = inputs;
+
   const onChange = (e) => {
-      setText(e.target.value);
-  }
+    const { value, name } = e.target; // 우선 e.target 에서 name 과 value 를 추출
+    setInputs({
+      ...inputs, // 기존의 input 객체를 복사한 뒤
+      [name]: value // name 키를 가진 값을 value 로 설정
+    });
+  };
 
   const onReset = () => {
-    setText('');
-  }
+    setInputs({
+      name: '',
+      nuckname: ''
+    })
+    nameInput.current.focus();
+  };
+
   return(
     <div>
-      <input onChange={onChange} value={text} />
+      <input
+        name="name"
+        placeholder="이름" 
+        onChange={onChange} 
+        value={name}
+        ref={nameInput} 
+      />
+      <input 
+        name="nickname"
+        placeholder="닉네임" 
+        onChange={onChange} 
+        value={nickname} 
+      />
       <button onClick={onReset}>초기화</button>
       <div>
-        <b>값: {text}</b>
+        <b>값: </b>
+        {name} ({nickname})
       </div>
     </div>
   )

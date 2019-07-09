a6679ace587b583e05e938b78e4a9c53771cb60e
gw1402535 <kis@gabia.com>
2019-07-09 12:56:33 +0900

    12 useRef

--- src/App.js
+++ src/App.js
@@ -2,9 +2,33 @@ import React from 'react';
 import UserList from './UserList';
 
 function App() {
-  return (
-    <UserList />
-  );
+  const users = [
+    {
+      id: 1,
+      username: 'velopert',
+      email: 'public.velopert@gmail.com'
+    },
+
+    {
+      id: 2,
+      username: 'tester',
+      email: 'tester@gmail.com'
+    },
+
+    {
+      id: 3,
+      username: 'liz',
+      email: 'liz@gmail.com'
+    },
+  ];
+
+  const nextId = userRef(4);
+  const onCreate = () => {
+
+
+    nextId.current += 1;
+  };
+  return <UserList users={users} />
 }
 
 export default App;
--- src/UserList.js
+++ src/UserList.js
@@ -8,27 +8,7 @@ function User({user}) {
   )
 }
 
-function UserList() {
-  const users = [
-    {
-      id: 1,
-      username: 'velopert',
-      email: 'public.velopert@gmail.com'
-    },
-
-    {
-      id: 2,
-      username: 'tester',
-      email: 'tester@gmail.com'
-    },
-
-    {
-      id: 3,
-      username: 'liz',
-      email: 'liz@gmail.com'
-    },
-  ];
-
+function UserList({users}) {
   return (
     <div>
       {users.map(user => (

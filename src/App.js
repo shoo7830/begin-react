2b73c0cfd78457b9133a95456964741c0b2efab3
gw1402535 <kis@gabia.com>
2019-07-16 16:43:49 +0900

    18.useCallback

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useRef, useState, useMemo } from 'react';
+import React, { useRef, useState, useMemo, useCallback } from 'react';
 import UserList from './UserList';
 import CreateUser from './CreateUser';
 
@@ -44,33 +44,40 @@ function App() {
   ]);
 
   const nextId = useRef(4);
-  const onCreate = () => {
+  const onCreate = useCallback(() => {
     const user = {
       id: nextId.current,
-      username,
+      username, 
       email
     };
+
     setUsers(users.concat(user));
-    
+
     setInputs({
       username: '',
-      email:''
+      email: ''
     });
 
     nextId.current += 1;
-  };
+  }, [users, username, email]);
 
-  const onRemove = id => {
-    setUsers(users.filter(user => user.id !== id));
-  };
+  const onRemove = useCallback(
+    id => {
+      setUsers(users.filter(user => user.id !== id));
+    }, 
+    [users]
+  );
 
-  const onToggle = id => {
-    setUsers(
-      users.map(user =>
-        user.id === id ? {...user, active: !user.active} :user
-      )
-    )
-  };
+  const onToggle = useCallback(
+    id => {
+      setUsers(
+        users.map(user =>
+          user.id === id ? { ...user, active: !user.active } : user
+        )
+      );
+    },
+    [users]
+  )
 
   const count = useMemo(()=>countActiveUsers(users), [users]);
   return (

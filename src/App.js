0c0d40abca4667059cbf8516ae35ec38efbc24d0
Aria Kim <shoo7830@gmail.com>
2019-07-17 23:37:12 +0900

    7장 컴포넌트의 라이프 사이클 메서드

--- src/App.js
+++ src/App.js
@@ -1,91 +1,29 @@
-import React, { useRef, useState, useMemo, useCallback } from 'react';
-import UserList from './UserList';
-import CreateUser from './CreateUser';
+import React, { Component} from 'react';
+import LifeCycleSample from './LifeCycleSample';
 
-function countActiveUsers(users) {
-  console.log('활성 사용자 수를 세는 중...');
-  return users.filter(user => user.active).length;
+function getRendomColor() {
+  return '#' + Math.floor(Math.random() * 16777215).toString(16);
 }
 
-function App() {
-  const [inputs, setInputs] = useState({
-    username: '',
-    email: ''
-  });
-  const {username, email} = inputs;
-  const onChange = useCallback(e => {
-    const { name, value } = e.target;
-    setInputs(inputs => ({
-      ...inputs,
-      [name]: value
-    }));
-  }, []);
-  const [users, setUsers] = useState([
-    {
-      id: 1,
-      username: 'velopert',
-      email: 'public.velopert@gmail.com',
-      active: true  // 초록색
-    },
+class App extends Component {
+  state = {
+    color: '#000000'
+  }
 
-    {
-      id: 2,
-      username: 'tester',
-      email: 'tester@gmail.com',
-      active: false
-    },
-
-    {
-      id: 3,
-      username: 'liz',
-      email: 'liz@gmail.com',
-      active: false
-    },
-  ]);
-
-  const nextId = useRef(4);
-  const onCreate = useCallback(() => {
-    const user = {
-      id: nextId.current,
-      username, 
-      email
-    };
-
-    setUsers(users => users.concat(user));
-
-    setInputs({
-      username: '',
-      email: ''
+  handleClick = () => {
+    this.setState({
+      color: getRendomColor()
     });
+  }
 
-    nextId.current += 1;
-  }, [username, email]);
-
-  const onRemove = useCallback(id => {
-      setUsers(users => users.filter(user => user.id !== id));
-    }, []);
-
-  const onToggle = useCallback(id => {
-      setUsers(users =>
-        users.map(user =>
-          user.id === id ? { ...user, active: !user.active } : user
-        )
-      );
-    }, []);
-
-  const count = useMemo(() => countActiveUsers(users), [users]);
-  return (
-      <>
-        <CreateUser
-          username={username}
-          email={email}
-          onChange={onChange}
-          onCreate={onCreate}
-        />
-        <UserList users={users} onRemove={onRemove} onToggle={onToggle} />
-        <div>활성사용자 수: {count}</div>
-        </>
-  );
+  render() {
+    return (
+        <div>
+          <button onClick={this.handleClick}>랜덤색상</button>
+          <LifeCycleSample color={this.state.color} />
+        </div>
+    )
+  }
 }
 
 export default App;
